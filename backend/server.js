3a8accfd71fe6727fd63847f50a5a51496a30946
mayankkk18 <anandmayank2202@gmail.com>
2024-06-19 16:17:48 +0530

    fixes deploy error

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,7 @@ const __dirname = path.resolve();
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-});
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
@@ -27,7 +25,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 
 
 connectToMongoDB()
